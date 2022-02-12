1c62c9c19572015c6920f7df8803ae8685d464cb
shamaunalam <shamaunalam@gmail.com>
2022-02-12 13:50:35 +0530

    third commit - added team html and media

--- IPL/settings.py
+++ IPL/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'IPL.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR,'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,8 +121,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
--- Teams/views.py
+++ Teams/views.py
@@ -1,6 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Teams,Players
+
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<center><h1>This is home!</h1></center>')
+    
+    team = Teams.objects.all()
+
+    return render(request,'team.html',{'teams':team})
+
+
+def TeamPage(request,pk):
+
+    team = Teams.objects.get(TeamId=pk)
+    players = Players.objects.filter(team=team)
+
+    return render(request,'team.html')
+
+
+
+    
